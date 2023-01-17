af06cbc6f418a7c72df3b756ad75441520b864e2
Scott Robbins <scott.robbins@mobiledefense.com>
2023-01-16 21:26:24 -0500

    Initial Commit

---
+++ DistributedTracingPoC/DistributedTracingPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistributedTracingPoCApp.swift
+//  DistributedTracingPoC
+//
+//  Created by Scott Robbins on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DistributedTracingPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
